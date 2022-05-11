769dd138f30de0e82ce7110508af3aa65c82a583
rocky3616 <105193000+rocky3616@users.noreply.github.com>
2022-05-11 13:23:41 +0530

    Add files via upload

---
+++ Hands-On/ForLoopM.java
@@ -0,0 +1,15 @@
+package corejava;
+
+public class ForLoopM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=2;
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(2+"*"+i+"  : "+2*i);
+		}
+
+	}
+
+}
