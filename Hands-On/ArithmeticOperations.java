6a5da4df57ad6a14450908c9a82899d5fe1dab83
rocky3616 <105193000+rocky3616@users.noreply.github.com>
2022-05-11 10:45:34 +0530

    Add files via upload

---
+++ Hands-On/ArithmeticOperations.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class ArithmeticOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1,n2;
+		n1=30;
+		n2=20;
+		System.out.println("number 1+number 2: "+(n1+n2));
+		System.out.println("number 1-number 2: "+ (n1-n2));
+		System.out.println("number 1*number 2: "+(n1*n2));
+		System.out.println("number 1/number 2: "+(n1/n2));
+		System.out.println("number 1%number 2: "+(n1%n2));
+
+	}
+
+}
