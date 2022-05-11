fa12511bd79e62e3674e50a5f78dc2c2ddf9c39f
rocky3616 <105193000+rocky3616@users.noreply.github.com>
2022-05-11 13:13:01 +0530

    Add files via upload

---
+++ Hands-On/IfLoopAge.java
@@ -0,0 +1,23 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class IfLoopAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter your age : ");
+		int age=sc.nextInt();
+		if(age>18)
+		{
+			System.out.println("You are eligible for voting...");
+		}
+		else
+		{
+			System.out.println("You are not eligible for voting.");
+		}
+
+	}
+
+}
