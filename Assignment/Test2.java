efbf4246da195bc1d37c966846eef7daa6fcc794
rocky3616 <105193000+rocky3616@users.noreply.github.com>
2022-05-25 23:24:18 +0530

    Add files via upload

---
+++ Assignment/Test2.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String str[] = { "Geeks", "for", "Geeks" };
+       	 for (int i = 0; i < str.length; i++)
+            System.out.print(str[i]);
+
+
+	}
+
+}
