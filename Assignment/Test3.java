b916eeea08e6886530cb358d187e284c07feb3a1
rocky3616 <105193000+rocky3616@users.noreply.github.com>
2022-05-25 23:27:50 +0530

    Add files via upload

---
+++ Assignment/Test3.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int arr[][] = { { 11, 22 }, { 33, 44, 55 } 
+		  };
+		         	 for (int i = 0; i < 2; i++) 
+		  {
+		              for (int j = 0; j < arr.length; j++)
+		              System.out.print(arr[i][j] + " ");
+		              System.out.println();
+		  }
+
+
+	}
+
+}
