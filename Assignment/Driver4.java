1efe63c1898d00d846af714d1494cc6ac7808d8e
rocky3616 <105193000+rocky3616@users.noreply.github.com>
2022-05-25 22:55:44 +0530

    Add files via upload

---
+++ Assignment/Driver4.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class Driver4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] intArr = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10};
+		 for (int i = 0; i < intArr . length ; i += 3) 
+		{ 
+		System . out . println (" Value : " + intArr [ i ]) ; 
+		} 
+
+
+	}
+
+}
