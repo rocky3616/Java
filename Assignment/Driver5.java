fafd4173005de4638af310dc553e5190058ad68c
rocky3616 <105193000+rocky3616@users.noreply.github.com>
2022-05-25 23:00:17 +0530

    Add files via upload

---
+++ Assignment/Driver5.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class Driver5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] intArr = new int [5]; 
+		for (int i = 0; i < intArr . length ; i ++) 
+		{
+		 intArr [ i ] = i * 2;
+		 }
+		 for (int i = 0; i < intArr . length ; i ++) 
+		{ 
+		System . out . print ( intArr [ i ] + ", ") ;
+		}
+
+	}
+
+}
