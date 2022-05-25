f23637376d999bf3274429431e563f2a22d4b4c5
rocky3616 <105193000+rocky3616@users.noreply.github.com>
2022-05-25 11:51:32 +0530

    Add files via upload

---
+++ Assignment/Driver.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] strArr=new String[10];
+		for(int i=strArr.length-1;i>=0;i--)
+		{
+			strArr[i]="b"+(i-1);
+		}
+		System.out.println("value : "+strArr[5]);
+
+	}
+
+}
